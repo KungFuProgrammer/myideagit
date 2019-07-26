fc2df04f7cf62644282d3a52371aed037711b652
乐乐乐 <504214263@qq.com>
2019-07-26 09:23:49 +0800

    修改了user，加了get，set方法

--- testGit/src/main/java/com/itheima/pojo/User.java
+++ testGit/src/main/java/com/itheima/pojo/User.java
@@ -1,4 +1,22 @@
 package com.itheima.pojo;
 
 public class User {
+    private String username;
+    private  String pawwword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPawwword() {
+        return pawwword;
+    }
+
+    public void setPawwword(String pawwword) {
+        this.pawwword = pawwword;
+    }
 }
